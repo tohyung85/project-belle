3160abe9c96d6fb1800d72882b913ac3c63b195d
tohyung85 <tantohyung@gmail.com>
2016-10-09 01:21:16 +0000

    Implement voice commands and termination

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,23 +2,98 @@ $(function(){
   if(!('webkitSpeechRecognition' in window)) {
     upgrade();
   } else {
-    // alert('speech recognition functionality here!');
-    var stop_speech = false;
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
+    var shutdown_ai = false;
+    var commands = ['message', 'email', 'notes'];
+    var grammar = '#JSGF V1.0; grammar commands; public <commands> = '+ commands.join('|') +' ;';
+    var speechRecognitionList = new webkitSpeechGrammarList();
+    speechRecognitionList.addFromString(grammar,1);
 
-    recognition.onstart = function() {}
+    var recognition = new webkitSpeechRecognition();
+    configureCommandRecognizer();
+
+    var stop_speech = false;
+    var emailVoiceRecognition = new webkitSpeechRecognition;
+    configureEmailRecognizer();
+  }
+
+  $('#start-record').click(startButton);
+  $('#stop-record').click(stopButton);
+
+  function startButton() {
+    final_transcript = '';
+    recognition.lang = "en-US";
+    recognition.start();
+  }
+
+  function stopButton() {
+    shutdown_ai = true;
+    recognition.stop();
+  }
+
+  function recordAudioInput () {
+    stopButton();    
+    emailVoiceRecognition.start();    
+  }
+
+  function configureCommandRecognizer () {
+    recognition.continuous = true;
+    recognition.interimResults = true;    
+    recognition.grammars = speechRecognitionList;
+
+    recognition.onstart = function() {
+      shutdown_ai = false;
+    }
     recognition.onresult = function(event) {
+      var last = event.results.length - 1;
+      var command = event.results[last][0].transcript;
+      var executed = '';
+      console.log(command);
+      switch (command) {
+        case ' email':
+        case 'email':
+          executed = 'email';
+          console.log('email executed');
+          recordAudioInput();
+          break;
+        case ' message':
+        case 'message':
+          executed = 'message';
+          console.log('message executed');
+          break;
+        case ' notes':
+        case 'notes':
+          executed = 'notes'
+          console.log('notes executed');
+          break;
+        default:
+          executed = 'invalid command';
+          console.log('no commands!');
+      }      
+    }
+    recognition.onerror = function() {}
+    recognition.onend = function() {      
+      if (shutdown_ai === true) {
+        console.log('shutting down...');  
+      } else {
+        console.log('still awake...');
+        recognition.start();  
+      }      
+    }
+  }
+
+  function configureEmailRecognizer () {
+    emailVoiceRecognition.continuous = true;
+    emailVoiceRecognition.interimResults = true;        
+
+    emailVoiceRecognition.onresult = function(event) {
       var interim_transcript = '';
 
       for (var i = event.resultIndex; i < event.results.length; i++) {
-        console.log(typeof event.results[i][0].transcript);
         if (event.results[i].isFinal) {
           console.log(event.results[i][0]);
           if (event.results[i][0].transcript.includes("okay stop")) {
             stop_speech = true;
-            recognition.stop();
+            emailVoiceRecognition.stop();
           }
           final_transcript += event.results[i][0].transcript;
         } else {
@@ -29,26 +104,27 @@ $(function(){
       // final_transcript = capitalize(final_transcript);
       final_span.innerHTML = final_transcript;
       interim_span.innerHTML = interim_transcript;
+    }    
+
+    emailVoiceRecognition.onstart = function() {
+      stop_speech = false;
     }
-    recognition.onerror = function() {}
-    recognition.onend = function() {      
+
+    emailVoiceRecognition.onend = function() {
       if (stop_speech === true) {
-        console.log('message end');  
+        emailVoiceRecognition.stop();
+        recognition.start();
+        console.log('email voice recognition end');
+        console.log('command voice recognition restarted');
       } else {
-        console.log('continue message....');
-        recognition.start();  
-      }      
+        console.log('continue message');
+        emailVoiceRecognition.start();
+      }    
+    }    
+
+    emailVoiceRecognition.onerror = function(event) {
+      console.log('Speech Recognition Error: '+ event.error);
     }
   }
-
-  $('#start-record').click(startButton);
-
-  function startButton(event) {
-    final_transcript = '';
-    recognition.lang = "en-US";
-    stop_speech = false;
-    recognition.start();
-  }
-
 
 });
